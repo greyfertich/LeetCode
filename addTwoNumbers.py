9d720eb5a85b7f6771fa32313355d404e4cc2c3a
Grey Fertich <greyfertich@gmail.com>
2018-10-01 14:45:36 -0400

    Faster runtime

--- addTwoNumbers.py
+++ addTwoNumbers.py
@@ -11,35 +11,23 @@ class Solution:
         :type l2: ListNode
         :rtype: ListNode
         """
-        num1 = []
-        num2 = []
-        current = l1
-        while True:
-            num1.append(current.val)
-            if current.next == None:
-                break
-            current = current.next
-        current = l2
-        while True:
-            num2.append(current.val)
-            if current.next == None:
-                break
-            current = current.next
-        num1 = list(reversed(num1))
-        num2 = list(reversed(num2))
-        st1 = ''
-        st2 = ''
-        for i in num1:
-            st1 += str(i)
-        for i in num2:
-            st2 += str(i)
-        new = list(str(int(st1) + int(st2)))
-        new.reverse()
-        link = ListNode(int(new[0]))
-        current = link
-        for i in range(len(new)):
-            if i > 0:
-                current.next = ListNode(int(new[i]))
-                current = current.next
-        return link
+        num1 = ""
+        num2 = ""
+        cur = l1
+        num1 += str(cur.val)
+        while cur.next:
+            cur = cur.next
+            num1 += str(cur.val)
+        cur = l2
+        num2 += str(cur.val)
+        while cur.next:
+            cur = cur.next
+            num2 += str(cur.val)
+        ans = str(int(num2[::-1]) + int(num1[::-1]))[::-1]
+        out = ListNode(int(ans[0]))
+        cur = out
+        for i in range(1,len(ans)):
+            cur.next = ListNode(int(ans[i]))
+            cur = cur.next
+        return out
                 
